c3e6921b2aeb9c9888d5ca836f12a7e487c10bb2
Ben Higginbotham <bicycleben88@gmail.com>
2021-02-18 09:25:39 -0500

    finished day 17

---
+++ sort-without-article/app.js
@@ -0,0 +1,32 @@
+const bandsList = document.querySelector("#bands");
+
+const bands = [
+  "The Plot in You",
+  "The Devil Wears Prada",
+  "Pierce the Veil",
+  "Norma Jean",
+  "The Bled",
+  "Say Anything",
+  "The Midway State",
+  "We Came as Romans",
+  "Counterparts",
+  "Oh, Sleeper",
+  "A Skylit Drive",
+  "Anywhere But Here",
+  "An Old Dog",
+];
+
+const strip = (bandName) => {
+  return bandName.replace(/^(a |the |an )/i, "").trim();
+};
+
+const sorted = bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
+
+bandsList.innerHTML = sorted
+  .map(
+    (band) =>
+      `
+            <li>${band}</li>
+        `
+  )
+  .join("");
