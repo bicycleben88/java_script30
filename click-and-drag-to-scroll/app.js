b8ce0e72b1466a7ae49aeec0ce4207c666300a64
Ben Higginbotham <bicycleben88@gmail.com>
2021-02-26 18:51:10 -0500

    finish day 27

---
+++ click-and-drag-to-scroll/app.js
@@ -0,0 +1,29 @@
+const slider = document.querySelector(".items");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+function setUpWalk(e) {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+}
+
+function walk(e) {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 4;
+  slider.scrollLeft = scrollLeft - walk;
+}
+
+function removeClass() {
+  isDown = false;
+  slider.classList.remove("active");
+}
+
+slider.addEventListener("mousedown", setUpWalk);
+slider.addEventListener("mousemove", walk);
+slider.addEventListener("mouseleave", removeClass);
+slider.addEventListener("mouseup", removeClass);
